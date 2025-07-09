f8d81afb413f07879a14e72669eb481236719ebe
TusharOnPoint <tushar23das@gmail.com>
2025-07-10 01:30:42 +0600

    Updated version of portfolio

---
+++ Portfolio2/script.js
@@ -0,0 +1,20 @@
+// Scroll highlight (optional for scroll active nav)
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
+
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
