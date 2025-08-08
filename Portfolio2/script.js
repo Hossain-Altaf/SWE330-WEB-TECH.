d8b8953175f6e6400cb818e5500f722144127adc
TusharOnPoint <tushar23das@gmail.com>
2025-08-09 04:03:31 +0600

    updated several things

--- Portfolio2/script.js
+++ Portfolio2/script.js
@@ -18,3 +18,40 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+
+  document.getElementById("read-more-btn").addEventListener("click", function (e) {
+    e.preventDefault();
+    const moreText = document.getElementById("more-text");
+    const btn = this;
+
+    if (moreText.style.display === "none") {
+      moreText.style.display = "block";
+      btn.textContent = "Read Less";
+    } else {
+      moreText.style.display = "none";
+      btn.textContent = "Read More";
+    }
+  });
+
+  document.getElementById('photo').addEventListener('click', function() {
+    const list = document.getElementById('photography');
+    list.style.display = list.style.display === 'none' ? 'block' : 'none';
+  });
+
+  document.getElementById('traveling-item').addEventListener('click', function() {
+    const list = document.getElementById('traveling-list');
+    list.style.display = list.style.display === 'none' ? 'block' : 'none';
+  });
+
+  document.getElementById('reading-item').addEventListener('click', function() {
+    const list = document.getElementById('reading-list');
+    list.style.display = list.style.display === 'none' ? 'block' : 'none';
+  });
+
+  document.getElementById('cinema-item').addEventListener('click', function() {
+    const list = document.getElementById('cinema-list');
+    list.style.display = list.style.display === 'none' ? 'block' : 'none';
+  });
+
+
